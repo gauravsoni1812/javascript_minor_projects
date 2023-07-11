26d83b26d68a3d889ee1815e8c5b4d0694aaa7d2
Gaurav Soni <75882626+gauravsoni1812@users.noreply.github.com>
2023-07-11 09:40:27 +0530

    Add files via upload

---
+++ random color generator/main.js
@@ -0,0 +1,14 @@
+const getColor = () => {
+    console.log("button clicked");
+    const red = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+  
+    const randomCode = `rgba(${red},${blue},${green},1)`;
+    document.body.style.backgroundColor = randomCode;
+  
+    document.getElementById("color-code").textContent = randomCode;
+  };
+  
+  document.getElementById("btn").addEventListener("click", getColor);
+  
