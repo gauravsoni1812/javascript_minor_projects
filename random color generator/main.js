38a2a6f53779f64cbd80c8c16ea0e4374d27b3ad
Gaurav Soni <75882626+gauravsoni1812@users.noreply.github.com>
2023-07-11 10:01:04 +0530

    Update main.js

--- random color generator/main.js
+++ random color generator/main.js
@@ -1,14 +1,13 @@
 const getColor = () => {
-    console.log("button clicked");
-    const red = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-  
-    const randomCode = `rgba(${red},${blue},${green},1)`;
-    document.body.style.backgroundColor = randomCode;
-  
-    document.getElementById("color-code").textContent = randomCode;
-  };
-  
-  document.getElementById("btn").addEventListener("click", getColor);
-  
+  console.log("button clicked");
+  const red = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+
+  const randomCode = `rgba(${red},${blue},${green},1)`;
+  document.body.style.backgroundColor = randomCode;
+
+  document.getElementById("color-code").textContent = randomCode;
+};
+
+document.getElementById("btn").addEventListener("click",getColor);
